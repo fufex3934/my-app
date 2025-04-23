c6e1308d2d955c7eefcbdef1dd1ae6c4fc57260b
fufex3934 <fufawakgari174@gmail.com>
2025-04-23 13:53:15 +0300

    creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ProductCard from './components/ProductCard.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <ProductCard/>
 
     </div>
   );
---
+++ src/components/ProductCard.js
@@ -0,0 +1,21 @@
+export default function ProductCard(){
+  return(
+    <article>
+      <h2>iPhone 15 Pro</h2>
+      <img
+      src="images/iphone.png"
+      alt="iphone 15 pro"
+      width={128}
+      height={128}
+      />
+      <p>Specification:</p>
+      <ul>
+        <li>A17 Pro chip with 6 core</li>
+        <li>A17 Pro chip with 6 core</li>
+        <li>A17 Pro chip with 6 core</li>
+        <li>A17 Pro chip with 6 core</li>
+      </ul>
+      <button>Buy</button>
+    </article>
+  )
+}
